e23d1dd654a6f268191b2610f21fc59784bd6688
sre1923 <59494713+sre1923@users.noreply.github.com>
2020-01-08 17:29:07 -0600

    Add files via upload

---
+++ Shortcut.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from collections import defaultdict
+
+class Graph():
+    def __init__(self):
+        """
+        self.edges is a dict of all possible next nodes
+        e.g. {'X': ['A', 'B', 'C', 'E'], ...}
+        self.weights has all the weights between two nodes,
+        with the two nodes as a tuple as the key
+        e.g. {('X', 'A'): 7, ('X', 'B'): 2, ...}
+        """
+        self.edges = defaultdict(list)
+        self.weights = {}
+    
+    def add_edge(self, from_node, to_node, weight):
+        # Note: assumes edges are bi-directional
+        self.edges[from_node].append(to_node)
+        self.edges[to_node].append(from_node)
+        self.weights[(from_node, to_node)] = weight
+        self.weights[(to_node, from_node)] = weight
+
+graph = Graph()
+
+edges = [
+    ('X', 'A', 7),
+    ('X', 'B', 2),
+    ('X', 'C', 3),
+    ('X', 'E', 4),
+    ('A', 'B', 3),
+    ('A', 'D', 4),
+    ('B', 'D', 4),
+    ('B', 'H', 5),
+    ('C', 'L', 2),
+    ('D', 'F', 1),
+    ('F', 'H', 3),
+    ('G', 'H', 2),
+    ('G', 'Y', 2),
+    ('I', 'J', 6),
+    ('I', 'K', 4),
+    ('I', 'L', 4),
+    ('J', 'L', 1),
+    ('K', 'Y', 5),
+]
+
+for edge in edges:
+    graph.add_edge(*edge)
+    
+def dijsktra(graph, initial, end):
+    # shortest paths is a dict of nodes
+    # whose value is a tuple of (previous node, weight)
+    shortest_paths = {initial: (None, 0)}
+    current_node = initial
+    visited = set()
+    
+    while current_node != end:
+        vis(current_node, weight)
+        
+        next_destinations = {node: shortest_paths[node] for node in shortest_paths if node not in visited}
+        if not next_destinations:
+            return "Route Not Possible"
+           path = path[::-1]
+    return path
+
+print(dijsktra(graph, 'X', 'Y'))
+
